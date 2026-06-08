329c0d585c3b8334cbdb8d3e4a87a73b6a22fcd2
Kenny <260030629+kennowiski@users.noreply.github.com>
2026-06-08 19:57:18 -0300

    Add Last.fm API handler to fetch recent tracks

---
+++ api/lastfm.js
@@ -0,0 +1,96 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  const lastfm_key = process.env.LASTFM_API_KEY;
+  const lastFmUser = 'kennowiski';
+
+  if (!lastfm_key) {
+    return res.status(500).json({
+      error: 'Missing LASTFM_API_KEY environment variable',
+    });
+  }
+
+  function getBestImage(track) {
+    const fallbackImage =
+      'https://s.ltrbxd.com/static/img/empty-poster-250.8491d904.png';
+
+    if (!track.image || !Array.isArray(track.image)) {
+      return fallbackImage;
+    }
+
+    const imageData =
+      track.image.find((img) => img.size === 'extralarge' && img['#text']) ||
+      track.image.find((img) => img.size === 'large' && img['#text']) ||
+      track.image.find((img) => img['#text']);
+
+    if (!imageData || !imageData['#text']) {
+      return fallbackImage;
+    }
+
+    return imageData['#text'].replace(/\/i\/u\/[^/]+\//, '/i/u/300x300/');
+  }
+
+  function formatTrack(track) {
+    const isNowPlaying =
+      track['@attr'] && track['@attr'].nowplaying === 'true';
+
+    return {
+      title: track.name || 'Desconhecido',
+      artist:
+        track.artist && track.artist['#text']
+          ? track.artist['#text']
+          : 'Desconhecido',
+      album:
+        track.album && track.album['#text']
+          ? track.album['#text']
+          : '',
+      albumImageUrl: getBestImage(track),
+      isNowPlaying,
+      playedAt:
+        track.date && track.date.uts
+          ? Number(track.date.uts) * 1000
+          : null,
+      url: track.url || null,
+    };
+  }
+
+  try {
+    const lastFmUrl =
+      `https://ws.audioscrobbler.com/2.0/` +
+      `?method=user.getrecenttracks` +
+      `&user=${encodeURIComponent(lastFmUser)}` +
+      `&api_key=${encodeURIComponent(lastfm_key)}` +
+      `&format=json` +
+      `&limit=10`;
+
+    const response = await fetch(lastFmUrl);
+
+    if (!response.ok) {
+      return res.status(response.status).json({
+        error: 'Failed to fetch Last.fm recent tracks',
+      });
+    }
+
+    const data = await response.json();
+
+    const rawTracks = data?.recenttracks?.track;
+
+    if (!Array.isArray(rawTracks)) {
+      return res.status(200).json({
+        provider: 'lastfm',
+        tracks: [],
+      });
+    }
+
+    const tracks = rawTracks.map(formatTrack);
+
+    return res.status(200).json({
+      provider: 'lastfm',
+      tracks,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+}
